e2a348727c6e62a9c64c7dbd5e904e607c05c900
atul <atulverma2861@gmail.com>
2025-09-20 08:57:01 +0530

    fix

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://seven-healer-backend.vercel.app","http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
